d80e1a58c8d4889b572f8f8b6fa6e3103ff3d13d
Jackie Ouzman <jackie.ouzman@csiro.au>
2018-10-10 17:11:07 +1030

    add 2008

--- nz_vine_scripts.R
+++ nz_vine_scripts.R
@@ -70,58 +70,118 @@ Cropload <- labels_Cropload
 levels(yld_comp$Cropload)[levels(yld_comp$Cropload)=="2c"] <- "2 cane pruned"
 levels(yld_comp$Cropload)[levels(yld_comp$Cropload)=="4c"] <- "4 cane pruned"
 
+#YES
 ggplot(yld_comp, aes(vine_size.2005, Calculated_yield_per_vine_kg_Harvest, colour = year_fac))+
   geom_point(alpha=0.6, position = position_jitter(width=0.2))+
-  stat_summary(fun.y = mean, geom = "point", colour = "black")+
+  geom_boxplot(alpha=0.6)+
+  #stat_summary(fun.y = mean, geom = "point", colour = "black")+
   scale_colour_manual(values = c("red", "green", "blue"))+
   #stat_summary(fun.data = mean_sdl, fun.args = list(mult =1) , 
   #geom = "errorbar", width = 0.2)+
-  facet_wrap(.~ Cropload) +
+  facet_wrap(year_fac ~ Cropload) +
   theme_bw()+
   labs(title ="Calculated yield vs vine size",
        x = "vine size",
        y = "Calculated yield per vine kg",
        colour = "year")
+str(yld_comp)
 
-ggplot(yld_comp, aes(vine_size.2005, Total_yield_per_vine_kg_Harvest, colour = year_fac))+
+ggplot(yld_comp, aes(vine_size.2005, Total_yield_per_vine_kg_Harvest , colour = year_fac))+
   geom_point(alpha=0.6, position = position_jitter(width=0.2))+
-  stat_summary(fun.y = mean, geom = "point", colour = "black")+
-  #stat_summary(fun.data = mean_sdl, fun.args = list(mult =1) , 
-  #geom = "errorbar", width = 0.2)+
+  geom_boxplot(alpha=0.6)+
   scale_colour_manual(values = c("red", "green", "blue"))+
-  facet_wrap(.~ Cropload) +
+  facet_wrap(year_fac ~ Cropload) +
   theme_bw()+
   labs(title ="Total yield vs vine size",
-       x = "vine sieze",
-       y = "Total yield per vine kg",
-       colour = "year")
+       x = "vine size",
+       y = "Total yield per vine in kg",
+       colour = "Year")
 
-ggplot(yld_comp, aes(year, Calculated_yield_per_vine_kg_Harvest, colour= year_fac))+
-  geom_boxplot(alpha = 0.5)+
+
+
+
+
+
+
+
+
+
+# see if I can show interaction
+
+ggplot(yld_comp, aes(year, Calculated_yield_per_vine_kg_Harvest, group = Cropload, colour = Cropload))+
+  geom_smooth(method = lm, se = FALSE)+
   geom_point()+
-  scale_colour_manual(values = c("red", "green", "blue"))+
-  facet_wrap(. ~ Cropload) +
+  #geom_point(colour = mycolour)+ #this is same colour for all my points
+  #scale_colour_manual(values = vine_size_Palette)+ #this is colours for my geom_point of geom_line from the defined above
+  scale_colour_manual(values = c("red", "blue"))+ # this is using default colours
   theme_bw()+
   scale_x_continuous(breaks=c(2005, 2006, 2007))+
   labs(title ="Calculated yield vs year",
        x = "year",
-       y = "Calculated yield per vine kg", 
-       colour = "year")
+       y = "Calculated yield per vine kg",
+       colour = "Crop load")
 
 
-ggplot(yld_comp, aes(year, Total_yield_per_vine_kg_Harvest, colour= year_fac))+
-  geom_boxplot(alpha = 0.5)+
+
+#build a model to look at the relationship bewteen yield year and crop load
+
+yld_yr_cropload_lm_add <- lm(Calculated_yield_per_vine_kg_Harvest ~ Cropload + year_fac, 
+                             data = yld_comp)
+anova(yld_yr_cropload_lm_add)
+lm_add_anova_output <- print(anova(yld_yr_cropload_lm_add))
+glimpse(lm_add_anova_output)
+
+lm_add_anova_output$`Sum Sq` <- round(lm_add_anova_output$`Sum Sq`, 2)
+lm_add_anova_output$`Mean Sq` <- round(lm_add_anova_output$`Mean Sq`, 2)
+lm_add_anova_output$`F value` <- round(lm_add_anova_output$`F value`, 2)                             
+
+#lm_add_anova_output$`Pr(>F)` <- round(lm_add_anova_output$`Pr(>F)`, 10) 
+
+emmeans(yld_yr_cropload_lm_add, pairwise ~ year_fac|Cropload)
+
+plot(yld_yr_cropload_lm_add, which = 1:2)
+
+##Factorial 
+
+yld_yr_cropload_lm_fac <- lm(Calculated_yield_per_vine_kg_Harvest ~ Cropload*year_fac, data = yld_comp)
+
+anova(yld_yr_cropload_lm_fac)
+lm_fact_anova_output <- print(anova(yld_yr_cropload_lm_fac))
+glimpse(lm_fact_anova_output)
+
+lm_fact_anova_output$`Sum Sq` <- round(lm_fact_anova_output$`Sum Sq`, 2)
+lm_fact_anova_output$`Mean Sq` <- round(lm_fact_anova_output$`Mean Sq`, 2)
+lm_fact_anova_output$`F value` <- round(lm_fact_anova_output$`F value`, 2)                             
+
+#lm_fact_anova_output$`Pr(>F)` <- round(lm_fact_anova_output$`Pr(>F)`, 10) 
+
+emmeans(yld_yr_cropload_lm, pairwise ~ year_fac|Cropload)
+
+
+
+##Factorial with blocking
+yld_yr_cropload_lmer <- lmer(Calculated_yield_per_vine_kg_Harvest ~ Cropload * year_fac 
+                        +(1|vine), data=yld_comp)
+anova(yld_yr_cropload)
+emmeans(yld_yr_cropload, pairwise~Cropload|year_fac)
+emmeans(yld_yr_cropload, pairwise~year_fac|Cropload)
+emmeans(yld_yr_cropload, consec ~year_fac|Cropload)
+
+
+
+
+ggplot(yld_comp, aes(year, Total_yield_per_vine_kg_Harvest, group = Cropload, colour = Cropload))+
+  geom_smooth(method = lm, se = FALSE)+
   geom_point()+
-  scale_colour_manual(values = c("red", "green", "blue"))+
-  facet_wrap(. ~ Cropload) +
+  #geom_point(colour = mycolour)+ #this is same colour for all my points
+  #scale_colour_manual(values = vine_size_Palette)+ #this is colours for my geom_point of geom_line from the defined above
+  scale_colour_manual(values = c("red", "blue"))+ # this is using default colours
   theme_bw()+
   scale_x_continuous(breaks=c(2005, 2006, 2007))+
   labs(title ="Total yield vs year",
        x = "year",
-       y = "Total yield per vine kg", 
-       colour = "year")
-
-
+       y = "Total yield per vine kg",
+       colour = "Crop load")
 
 
 
@@ -138,18 +198,28 @@ ggplot(yld_comp, aes(vine_size.2005, Total_number_bunches_per_vineHarvest, colou
        y = "number of bunches",
        colour = "year")
 
-ggplot(yld_comp, aes(vine_size.2005, bunch_weight_good_bunches_g_Harvest, colour = year_fac))+
-  geom_point(alpha=0.6, position = position_jitter(width=0.2))+
-  stat_summary(fun.y = mean, geom = "point", colour = "black")+
-  #stat_summary(fun.data = mean_sdl, fun.args = list(mult =1) , 
-  #geom = "errorbar", width = 0.2)+
-  scale_colour_manual(values = c("red", "green", "blue"))+
-  facet_wrap(.~ Cropload) +
+ggplot(yld_comp, aes(vine_size.2005, bunch_weight_good_bunches_g_Harvest, colour = Cropload))+
+  geom_boxplot(alpha = 0.5)+
+  geom_point(alpha = 0.2)+
+  scale_colour_manual(values = c("red", "blue"))+
+  facet_wrap(Cropload ~ year_fac) +
   theme_bw()+
   labs(title ="Bunch weight yield vs vine size",
        x = "vine size",
        y = "Bunch weight (grams)",
-       colour = "year")
+       colour = "Crop load")
+
+ggplot(yld_comp, aes(year, bunch_weight_good_bunches_g_Harvest, colour = Cropload))+
+  geom_boxplot(alpha = 0.5)+
+  #geom_point()+
+  scale_colour_manual(values = c("black", "red", "green", "yellow", "blue"))+
+  facet_wrap(. ~ vine_size.2005) +
+  scale_x_continuous(breaks=c(2005, 2006, 2007))+
+  theme_bw()+
+  labs(title ="Bunch weight yield vs vine size",
+       x = "Year",
+       y = "Bunch weight (grams)",
+       colour = "Crop load")
 
 str(yld_comp)
 
@@ -208,16 +278,5 @@ vine_size_Palette <- c("#000000", "#CC0000", "#66CC00", "#FFFF00", "#0000FF")
 #year_Palette 
 
 
-ggplot(yld_comp, aes(year, Calculated_yield_per_vine_kg_Harvest, group = Cropload, colour = Cropload))+
-  geom_smooth(method = lm, se = FALSE)+
-  geom_point()+
-  #geom_point(colour = mycolour)+ #this is same colour for all my points
-  #scale_colour_manual(values = vine_size_Palette)+ #this is colours for my geom_point of geom_line from the defined above
-  scale_colour_manual(values = c("red", "blue"))+ # this is using default colours
-  theme_bw()+
-  scale_x_continuous(breaks=c(2005, 2006, 2007))+
-  labs(title ="Calculated yield vs year",
-       x = "year",
-       y = "Calculated yield per vine kg")
 
 
